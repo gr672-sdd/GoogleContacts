4feda9e257f5d6a5339684a78ae1a4ff0540b2ad
Sorokin_Daniil <daniil_sorokin_2000@mail.ru>
2024-11-02 02:37:20 +0700

    Start

---
+++ SorokinDotNetTest/CreateContact.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.PeopleService.v1.Data;
+using SorokinDotNetTest.ViewModels;
+
+namespace SorokinDotNetTest
+{
+    public partial class CreateContact : Window
+    {
+        public CreateContact(UserCredential credential, Person contact, string actionType)
+        {
+            InitializeComponent();
+
+            // Привязываем DataContext к соответствующей ViewModel
+            DataContext = new CreateContactViewModel(credential, contact, actionType);
+        }
+    }
+}
---
+++ SorokinDotNetTest/MainWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿using SorokinDotNetTest.ViewModels;
+using System.Windows;
+
+namespace SorokinDotNetTest
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new AuthorizationViewModel(); // Привязываем DataContext
+        }
+    }
+}
